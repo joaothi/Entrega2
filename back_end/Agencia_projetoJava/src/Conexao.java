82cda524d7dcfeeb671af4dcb2599572b60e7d84
joaothi <109032327+joaothi@users.noreply.github.com>
2022-09-05 23:31:39 -0300

    atualização do diretório SQL
    atualização do diretório do sql

--- back_end/Agencia_projetoJava/src/Conexao.java
+++ back_end/Agencia_projetoJava/src/Conexao.java
@@ -22,7 +22,7 @@ public class Conexao {
 
    //Dados de caminho, porta e nome da base de dados que irá ser feita a conexão
 
-   private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/crud";
+   private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/agencia";
    
 // * Cria uma conexão com o banco de dados MySQL utilizando o nome de usuário e senha fornecidos
 
